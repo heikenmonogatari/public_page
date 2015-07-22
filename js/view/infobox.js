1a33bc3ea46acc7a50f34150613a00e05c726ba1
Nicolas <nic.truong@gmail.com>
2015-07-22 17:06:48 -0400

    CSS and infoxbox change

--- js/view/infobox.js
+++ js/view/infobox.js
@@ -2,6 +2,10 @@ var InfoboxItemView = Backbone.Marionette.ItemView.extend({
 
 	template: "#infobox_template",
 
+	events: {
+		"click .close" : "closeInfobox"
+	},
+
 	initialize: function(options) {
 		this.map = options.map;
 	},
@@ -29,31 +33,110 @@ var InfoboxItemView = Backbone.Marionette.ItemView.extend({
 				self.$('.chart_container').highcharts({
 					chart: {
 			        	type: 'column',
+			        	backgroundColor: "#282828"
 			        },
 			        title: {
-			            text: "HELLO CHART"
+			            text: "Daily Counts",
+			            style: {
+			            	color: "#d4d600"
+			            }
+			        },
+			        subtitle: {
+			        	text: moment().subtract(1, 'M').format('YYYY-MM-DD') + " to " + moment().subtract(1, 'd').format('YYYY-MM-DD'),
+			        	style: {
+			            	color: "#d4d600"
+			            }
 			        },
 			        xAxis: {
 			            type: 'datetime',
-			            minRange: 24 * 3600000 
+			            minRange: 24 * 3600000,
+			            labels: {
+				       		enabled: false
+					    },
+					    minorTickLength: 0,
+					    tickLength: 0
+			        },
+			        yAxis: {
+			        	labels: {
+				       		enabled: true
+					    },
+					    minorTickLength: 0,
+					    tickLength: 0,
+					    title: {
+					    	text: null
+					    }
+			        },
+			        tooltip: {
+			        	formatter: function() {
+			        		return moment(this.x).format('YYYY-MM-DD') + ": " + this.y;
+			        	}
 			        },
 			        legend: {
 			            enabled: false
 			        },
 			        plotOptions: {
-			            area: {
-			                lineWidth: 6,
-			            },
 			            series: {
-			            	animation: false
+			            	animation: false,
+			            	borderWidth: 0 
 			            }	         
 			        },
 			        series: [{
 			            data: serialized_data,
-			            pointWidth: 10
+			            pointWidth: 2,
+			            color: "#d4d600"
 			        }]
 				})
 			}
 		});
+	
+		$(function() {
+			$("#chart-area").draggable({
+				containment: "parent"
+			});
+		});
+
+		this.$el.hide().fadeIn();
+
+	},
+
+	closeInfobox: function() {
+		var self = this;
+
+		this.$el.fadeOut(function(){
+			$(this).remove();
+		}); 
+	},
+
+	onShow: function() {
+		var photos = this.model.get('photo');
+
+		$('#chart-area').children(":first").append('<div id="myCarousel" class="carousel slide" data-ride="carousel">')
+
+		$('#myCarousel').append('<ol class="carousel-indicators">');
+
+		for (var i=0; i<photos.length; i++) {
+			if (i == 0) {
+				$('.carousel-indicators').append('<li data-target="#myCarousel" data-slide-to="'+ i + '" class="active"></li>');	
+			}else{
+				$('.carousel-indicators').append('<li data-target="#myCarousel" data-slide-to="'+ i + '" class=""></li>');
+			}
+		}
+
+		$('#myCarousel').append('<div class="carousel-inner" role="listbox"></div>');
+
+		for (var i=0; i<photos.length; i++) {
+			if (i == 0) {
+				$('.carousel-inner').append('<div class="item active" id="item' + i + '"></div>');
+			}else{
+				$('.carousel-inner').append('<div class="item" id="item' + i + '"></div>');
+			}
+			$('#item' + i).append('<img class="photoCarousel" src="' + photos[i] + '">');
+		}
+
+		$('#myCarousel').append('<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">');
+		$('.left').append('<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span>');
+
+		$('#myCarousel').append('<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">');
+		$('.right').append('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span>');
 	}
 });
